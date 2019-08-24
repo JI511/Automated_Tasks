35e04e378fbf0f77533f5a45b0fb7a8d3f10fe3c
John Ingwersen <johningwersen11@gmail.com>
2019-08-24 18:03:19 -0500

    Start of script to check if git repo needs to be updated or initialized

---
+++ check_for_modifications.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# checkout specific repos or update if already exists
+
+if [ ! -d "/home/pi/Desktop/Programs/Personal_Fitness" ]
+then
+    echo "exist"
+else
+    echo "does not exist"

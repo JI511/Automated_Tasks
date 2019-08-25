62de2d809dcc6e008d68207a80279fd812d1600f
John Ingwersen <johningwersen11@gmail.com>
2019-08-24 20:36:40 -0500

    write to file with status updates

--- is_failure_notification_needed.sh
+++ is_failure_notification_needed.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 # reads the unit test output log to see if failures occurred. Will notify user if true.
 
+echo "starting.." > status.txt
 /usr/bin/python3 check_log_file.py ../Personal_Fitness/logs.txt config.txt
 if [ $? -eq 1 ]
 then
-  echo Error!
+  echo "Error!" >> status.txt
+else
+  echo "Looks good!" >> status.txt
 fi

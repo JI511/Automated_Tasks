1c2373bd7b10360dd9abf9894f8be7ac77fb5bbe
John Ingwersen <johningwersen11@gmail.com>
2019-08-24 19:41:40 -0500

    update

--- check_log_file.py
+++ check_log_file.py
@@ -9,30 +9,34 @@ from email.message import EmailMessage
 def main(log_path, pass_path):
     my_log_path = str(log_path)
     my_pass_path = str(pass_path)
-    if os.path.exists(my_log_path) and my_log_path.endswith('.txt'):
+    if os.path.exists(my_log_path) and my_log_path.endswith('.txt') and \
+            os.path.exists(my_pass_path) and my_pass_path.endswith('.txt'):
         try:
             p_file = open(my_pass_path, 'r')
-            gmail_sender = 'johnspibot@gmail.com'
-            recipient = 'johningwersen11@gmail.com'
             gmail_pass = p_file.read()
+            p_file.close()
             file = open(my_log_path, 'r')
             for line in file.readlines():
                 if 'ERROR' in line or 'FAIL' in line:
-                    msg = EmailMessage()
-                    msg.set_content('Unit test failure!')
-                    msg['Subject'] = 'Personal_Fitness Repo Test Failure'
-                    msg['From'] = gmail_sender
-                    msg['To'] = recipient
-                    server = smtplib.SMTP('smtp.gmail.com', 587)
+                    gmail_sender = 'johnspibot@gmail.com'
+                    recipient = 'johningwersen11@gmail.com'
+                    subject = 'Personal_Fitness Repo Test Failure'
+                    msg_body = 'Unit test failure!'
+                    body = '\r\n'.join(['To: %s' % recipient,
+                                        'From: %s' % gmail_sender,
+                                        'Subject: %s' % subject,
+                                        '', msg_body])
+                    server = smtplib.SMTP(host='smtp.gmail.com', port=13268)
                     server.ehlo()
                     server.starttls()
                     server.login('johnspibot@gmail.com', gmail_pass)
                     try:
-                        server.sendmail(gmail_sender, recipient, msg)
+                        server.sendmail(gmail_sender, [recipient], body)
                     except:
-                        pass
+                        print("There was an error")
                     server.quit()
                     sys.exit(1)
+            file.close()
             sys.exit(0)
         except Exception:
             raise

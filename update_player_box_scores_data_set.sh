7e600a12ea9d7c712827abb13185073c95d26374
John Ingwersen <johningwersen11@gmail.com>
2019-10-27 22:38:17 -0500

    Use python3

--- update_player_box_scores_data_set.sh
+++ update_player_box_scores_data_set.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 cd /home/pi/Desktop/Programs/NBA_Beautiful_Data
-python main.py --yesterday
+python3 main.py --yesterday
 git add player_box_scores.csv
 git commit -m "Daily player box score update from raspi"
 git push origin master

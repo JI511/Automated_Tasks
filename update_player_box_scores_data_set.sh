66bb1259cbe8f5591ef23465f1e6fbbf49439906
John Ingwersen <johningwersen11@gmail.com>
2019-10-27 22:36:36 -0500

    removed pip install

--- update_player_box_scores_data_set.sh
+++ update_player_box_scores_data_set.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 cd /home/pi/Desktop/Programs/NBA_Beautiful_Data
-pip3 install -r requirements.txt
 python main.py --yesterday
 git add player_box_scores.csv
 git commit -m "Daily player box score update from raspi"

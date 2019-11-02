5aaca7ade41274ea70d52d18b1bc061e4d1dc0c7
John Ingwersen <johningwersen11@gmail.com>
2019-11-02 10:32:48 -0500

    Updated args for main in NBA repo

--- update_player_box_scores_data_set.sh
+++ update_player_box_scores_data_set.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 cd /home/pi/Desktop/Programs/NBA_Beautiful_Data
-python3 main.py --yesterday --log
+python3 main.py --yesterday --log --gather_new
 git add player_box_scores.csv log.ini
 git commit -m "Daily player box score update from raspi"
 git push origin master
